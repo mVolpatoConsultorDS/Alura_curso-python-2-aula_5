c678a9e0fcc07b1da90e29ff3bf9efef73e3d9a3
Milton Volpato <volpato.milton@principal.com>
2024-01-30 19:14:25 -0300

    20240130 Aula Heranca

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+parto_paozinho = Prato('Paozinho', 2.00, 'O melhor pao da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(parto_paozinho)
 
 if __name__ == '__main__':
     main()
