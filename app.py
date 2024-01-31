959d2626bd0601b3b9e425f0bfff9e6474e6680e
Milton Volpato <volpato.milton@principal.com>
2024-01-31 17:41:19 -0300

    20240131 Aula 02

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
-parto_paozinho = Prato('Paozinho', 2.00, 'O melhor pao da cidade')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pao da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
     print(bebida_suco)
-    print(parto_paozinho)
+    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
+    print()
 
 if __name__ == '__main__':
     main()
